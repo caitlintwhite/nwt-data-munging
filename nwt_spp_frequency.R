7cd7bfd85dd6453a7b2a2454fa1a223cd8f2ab34
caitlintwhite <caitlin.t.white@colorado.edu>
2019-03-01 16:33:29 -0700

    start spp freq code

---
+++ nwt_spp_frequency.R
@@ -0,0 +1,383 @@
+# nwt spp frequency trends
+# borrowed from bosmp-spp-frequency analysis (belongs to ctw)
+# caitlin.t.white@colorado.edu
+
+
+# -- SETUP -----
+options(stringsAsFactors = FALSE)
+library(tidyverse)
+
+NA_vals <- c("NA", "", ".", " ")
+# start with codom plots
+codom_datapath <- "../../Documents/nwt_lter/unpub_data/NWT_CoDom_SpComp_data_L1.csv"
+codom <- read.csv(codom_datapath, na.strings = NA_vals)
+str(codom)
+summary(codom)
+# fix date column format (change from character to dateTime)
+codom$date <- as.Date(codom$date, format = "%m/%d/%y")
+# look at unique vals to be sure all looks okay
+lapply(codom, unique)
+
+# note: treatments are rem (removed) and nut (nutrient) add. full treatment is captured in "plot" col
+# need to create unique site_plot key for keeping track of spp
+# "meta" comm will be full plot treatment
+
+# create site key
+codom$sitekey <- paste(codom$site, codom$plot, sep = "_")
+# create vector of unk herbaceous spp
+unk_herbs <- sort(unique(codom$spp[grepl("_SP1",codom$spp)]))
+unk_herbs <- unk_herbs[!grepl("salix", unk_herbs, ignore.case = T)] # take out salix
+
+# create veg_only df
+codom_veg <- codom[!codom$spp %in% c("rock", "lichen", "litter", "bare", "moss") & # remove non-veg
+                     #!grepl("_SP1", codom$spp),] # remove all unknowns
+                     !codom$spp %in% unk_herbs,] # keep salix
+# confirm only species-ID'd names in spp list
+sort(unique(codom_veg$spp)) #yes
+
+# create spp lookup table
+codom_spp_lookup <- dplyr::select(codom, spp:growth_habit) %>%
+  distinct()
+# check for duplicates
+summary(duplicated(codom_spp_lookup$spp))
+summary(duplicated(codom_spp_lookup)) #no dups, okay to proceed
+
+# create treatment lookup tabls
+codom_trt_lookup <- dplyr::select(codom, local_site,site:nut, sitekey) %>%
+  distinct()
+# check for duplicates
+summary(duplicated(codom_trt_lookup$sitekey)) #no dups, okay to proceed
+
+
+# -- SPP GAINS, SITE-PLOT LEVEL -----
+# for-loop tracks new spp added on site-plot level data
+
+# build running tally of new spp 
+# initiate empty master data frame
+codom_spp_gains_siteplot_counts_allT <- data.frame()
+codom_spp_gains_siteplot_names_df <- data.frame()
+for(s in unique(codom_veg$sitekey)){
+    print(paste("looking at", s, "species gains..."))
+    
+    # create data frame for individual transect
+    plot_df <- codom_veg[codom_veg$sitekey == s,] %>%
+      mutate(event = as.numeric(as.factor(year))) %>%
+      filter(!(rem == "D" & spp == "DESCAE")) %>% # don't track descae in d removal plots
+      filter(!(rem == "A" & spp == "GEUROS")) # don't track geuros in a removal plots
+    
+    # create temporary df for storing new spp counts
+    temp_df <- data.frame(sitekey = s, event = unique(plot_df$event), year = unique(plot_df$year))
+    temp_df$new_spp <- NA
+    spp_list_running <- unique(plot_df$spp[plot_df$event ==1])
+    
+    for(i in 2:max(plot_df$event)){
+      # grab new native spp gained
+      event_spp <- unique(plot_df$spp[plot_df$event == i])
+      new_spp <- event_spp[!event_spp %in% spp_list_running]
+      
+      # store spp gained
+      if(length(new_spp) > 0){
+        temp_spp_gains_df <- data.frame(spp_gains = new_spp)
+      } else{
+        temp_spp_gains_df <- data.frame(spp_gains = NA)
+      }
+      
+      temp_spp_gains_df$event <- i
+      temp_spp_gains_df$year <- unique(plot_df$year[plot_df$event == i])
+      temp_spp_gains_df$sitekey <- s
+      print(paste(s, "event", i, "spp gained:", stringr::str_flatten(new_spp, collapse = ", ")))
+      
+      
+      # add new_spp to running spp_list
+      spp_list_running <- c(spp_list_running, new_spp)
+      
+      # add info to temp_df
+      temp_df$new_spp[temp_df$event == i] <- length(new_spp)
+      # add info to lost_spp_meta_df
+      codom_spp_gains_siteplot_names_df <- rbind(codom_spp_gains_siteplot_names_df, temp_spp_gains_df)
+    }
+    # add transect new spp to master new spp counts data frame
+    codom_spp_gains_siteplot_counts_allT <- rbind(codom_spp_gains_siteplot_counts_allT, na.omit(temp_df)) #na.omit removes event 1 since spp gained in that year will always be NA
+  }
+
+
+# check to be sure no codes duplicated at transect level
+with(codom_spp_gains_siteplot_names_df, lapply(split(spp_gains, sitekey), function(x) x[duplicated(x)])) #only NAs duplicated, which is fine (some yrs didn't lose any spp)
+# only NA! good
+
+# add in site and treatment cols
+codom_spp_gains_siteplot_counts_allT <- left_join(codom_spp_gains_siteplot_counts_allT, codom_trt_lookup)
+codom_spp_gains_siteplot_names_df <- left_join(codom_spp_gains_siteplot_names_df, codom_trt_lookup) %>%
+  left_join(codom_spp_lookup, by = c("spp_gains" = "spp"))
+  
+codom_spp_gains_fig <- ggplot(subset(codom_spp_gains_siteplot_counts_allT, !grepl("B",plot)), aes(year, new_spp)) +
+  geom_jitter(alpha = 0.6, width = 0.25, height = 0) +
+  geom_smooth(se = F) +
+  labs(title = "NWT LTER: number of new species, by codom treatment, added to plot species list with each sampling event",
+       subtitle = "Points within year jittered slightly; blue = plot-level trend, purple = meta-community species gains and trend ",
+       y = "Number of new species gained",
+       x = "Monitoring year") +
+  scale_x_continuous(breaks = seq(2002, 2018, by = 2)) +
+  #scale_y_continuous(breaks = seq(0,36,4)) + #expand = c(0.02,0.01), limits = c(0,NA), 
+  theme_light() +
+  #theme(plot.title = element_text(size = 10),
+  #      plot.subtitle = element_text(size = 9)) +
+  facet_grid(plot~.)
+
+
+# -- SPP GAINS IN META COMMUNITY -----
+# initiate empty master data frame
+codom_spp_gains_meta_counts_allT <- data.frame()
+codom_spp_gains_meta_names_df <- data.frame()
+for(p in unique(codom_veg$plot)){
+  print(paste("looking at", p, "species gains in meta community..."))
+  # create data frame for meta community
+  meta_df <- codom_veg[codom_veg$plot == p,] %>%
+    mutate(event = as.numeric(as.factor(year))) %>%
+    filter(!(rem == "D" & spp == "DESCAE")) %>% # don't track descae in d removal plots
+    filter(!(rem == "A" & spp == "GEUROS")) # don't track geuros in a removal plots
+  
+  
+  # create temporary df for storing new spp counts
+  temp_df <- data.frame(event = unique(meta_df$event), year = unique(meta_df$year))
+  temp_df$plot <- p
+  temp_df$new_spp <- NA
+  spp_list_running <- sort(unique(meta_df$spp[meta_df$event ==1 & meta_df$plot == p]))
+  
+  for(i in 2:max(meta_df$event)){
+    # grab new native spp gained
+    event_spp <- sort(unique(meta_df$spp[meta_df$event == i & meta_df$plot == p]))
+    new_spp <- event_spp[!event_spp %in% spp_list_running]
+    
+    # store spp gained
+    if(length(new_spp) > 0){
+      temp_spp_gains_df <- data.frame(spp_gains = new_spp)
+    } else{
+      temp_spp_gains_df <- data.frame(spp_gains = NA)
+    }
+    temp_spp_gains_df$plot <- p
+    temp_spp_gains_df$year <- unique(meta_df$year[meta_df$event == i])
+    temp_spp_gains_df$event <- i
+    print(paste("Event", i, p, "spp gained in system:", stringr::str_flatten(new_spp, collapse = ", ")))
+    
+    
+    # add new_spp to running spp_list
+    spp_list_running <- c(spp_list_running, new_spp)
+    
+    # add info to temp_df
+    temp_df$new_spp[temp_df$event == i & temp_df$plot == p] <- length(new_spp)
+    # add info to lost_spp_meta_df
+    codom_spp_gains_meta_names_df <- rbind(codom_spp_gains_meta_names_df, temp_spp_gains_df)
+  }
+  # add meta new spp to master new spp counts data frame
+  codom_spp_gains_meta_counts_allT <- rbind(codom_spp_gains_meta_counts_allT, na.omit(temp_df))
+}
+
+# check to be sure no codes duplicated
+with(codom_spp_gains_meta_names_df, lapply(split(spp_gains, plot), function(x) x[duplicated(x)])) #only NAs duplicated, which is fine (some yrs didn't lose any spp)
+# only NAs, good
+
+# add in site and treatment descriptive cols
+codom_spp_gains_meta_counts_allT <- left_join(codom_spp_gains_meta_counts_allT, distinct(dplyr::select(codom_trt_lookup, plot:nut)))
+codom_spp_gains_meta_names_df <- left_join(codom_spp_gains_meta_names_df, distinct(dplyr::select(codom_trt_lookup, plot:nut))) %>%
+  left_join(codom_spp_lookup, by = c("spp_gains" = "spp"))
+
+
+# add meta trend to transect spp gains fig
+codom_spp_gains_fig +
+  #scale_y_continuous(limits = c(0,NA)) +
+  geom_point(data = subset(codom_spp_gains_meta_counts_allT, !grepl("B",plot)), aes(year, new_spp), col = "purple") +
+  geom_smooth(data = subset(codom_spp_gains_meta_counts_allT,!grepl("B",plot)), aes(year, new_spp), col = "purple", se = F) # fill = "orchid"
+
+
+# -- SPP FREQ WITH CLEAN TRANSECTS ----
+# how many spp only occur once in the record on a transect?
+spp_freq.clean <- veg_dat[veg_dat$Lifeform != "Ground cover", c("Year", "Area", "sitekey", "spp")] %>%
+  group_by(sitekey) %>%
+  mutate(yrs_sampled = length(unique(Year))) %>%
+  ungroup() %>%
+  filter(!grepl("unk| sp", spp, ignore.case = T)) %>% #remove unknown spp
+  distinct() %>%
+  group_by(Area, sitekey, yrs_sampled, spp) %>%
+  summarise(nobs = length(spp))
+# what percent of spp, at the tranect level, only occur once in the whole record
+nrow(spp_freq.clean[spp_freq.clean$nobs == 1,])/nrow(spp_freq.clean) #27.7% (1% down from uncorrected transect IDs)
+
+# plot single-obs spp over time, by # yrs transect sampled (to compare to Ann's figure for GMAP)
+spp_freq_pct.clean <- spp_freq.clean %>%
+  mutate(N_once = ifelse(nobs == 1, 1, 0)) %>%
+  group_by(Area, sitekey, yrs_sampled, N_once) %>%
+  summarise(N_spp = length(spp)) %>%
+  ungroup() %>%
+  group_by(sitekey) %>%
+  mutate(total_spp = sum(N_spp)) %>%
+  ungroup() %>%
+  mutate(rel_pct = (N_spp/total_spp)*100) %>%
+  subset(N_once == 1)
+
+ggplot(spp_freq_pct.clean, aes(yrs_sampled, rel_pct)) +
+  geom_point(alpha = 0.6, size = 2) +
+  geom_smooth(method = "lm", lty = 2, col = "dodgerblue3") +
+  annotate("text", x = 22, y = 28, label = "n.s.", size = 4, col = "dodgerblue3") +
+  labs(title = "TGSNA: percent of species present only 1 year in entire record, transect-level",
+       subtitle = "Monitoring years: 1991-2018; trendline = overall trend (n.s.); corrected sitekey in 2004",
+       y = "Pct species present in only 1 year",
+       x = "Year monitored") +
+  scale_x_continuous(breaks = seq(20, 28, by = 1)) +
+  theme_light() +
+  theme(title = element_text(size = 10))
+
+
+
+# -- SPP LOST OVER TIME -----
+# what is the inverse? (spp lost over time)
+
+# build running tally of spp lost
+# initiate empty master data frame
+codom_spp_lost_siteplot_count_allT <- data.frame()
+codom_lost_spp_siteplot_names_df <- data.frame()
+for(s in unique(codom_veg$sitekey)){
+    print(paste("looking at", s, "species lost..."))
+    # create data frame for individual siteplot
+    siteplot_df <- codom_veg[codom_veg$sitekey == s,] %>%
+      mutate(event = as.numeric(as.factor(year))) %>%
+      filter(!(rem == "D" & spp == "DESCAE")) %>% # don't track descae in d removal plots
+      filter(!(rem == "A" & spp == "GEUROS")) # don't track geuros in a removal plots
+    
+    # create temporary df for storing new spp counts
+    temp_df <- data.frame(sitekey = s, event = unique(siteplot_df$event), year = unique(siteplot_df$year))
+    temp_df$lost_spp <- NA
+    spplist_lag1 <- unique(siteplot_df$spp[siteplot_df$event ==1])
+    
+    i <- 2
+    while(i %in% 2:max(siteplot_df$event)){
+      # grab full spp list for next year to end of sampling record
+      fwd_spplist <- unique(siteplot_df$spp[siteplot_df$event %in% (i+1):max(siteplot_df$event)])
+      
+      # grab native spp list for current sampling event
+      event_spp <- unique(siteplot_df$spp[siteplot_df$event == i])
+      # compare spp that disappeared from year prior
+      event_spp_loss <- spplist_lag1[!spplist_lag1 %in% event_spp]
+      # compare spp lost from year prior to future species list
+      event_spp_loss <- event_spp_loss[!event_spp_loss %in% fwd_spplist]
+      print(paste("event", i, s, "spp lost: ", stringr::str_flatten(event_spp_loss, collapse = ", ")))
+      
+      # store spp lost
+      if(length(event_spp_loss) > 0){
+        temp_spp_loss_df <- data.frame(lost_spp = event_spp_loss)
+      } else {
+        temp_spp_loss_df <- data.frame(lost_spp = NA)
+      }
+      
+      temp_spp_loss_df$year <- unique(siteplot_df$year[siteplot_df$event == i])
+      temp_spp_loss_df$event <- i
+      temp_spp_loss_df$sitekey <- s
+      
+      # update spp list to current year
+      spplist_lag1 <- event_spp
+      
+      # add info to temp_df
+      temp_df$lost_spp[temp_df$event == i] <- length(event_spp_loss)
+      
+      # add info to codom_lost_spp_siteplot_df
+      codom_lost_spp_siteplot_names_df <- rbind(codom_lost_spp_siteplot_names_df, temp_spp_loss_df)
+      i <- i+1
+    }
+    # add siteplot new spp to master new spp counts data frame
+    codom_spp_lost_siteplot_count_allT <- rbind(codom_spp_lost_siteplot_count_allT, na.omit(temp_df))
+  }
+
+# check to be sure no codes duplicated at siteplot level
+with(codom_lost_spp_siteplot_names_df, lapply(split(lost_spp, sitekey), function(x) x[duplicated(x)])) #only NAs duplicated, which is fine (some yrs didn't lose any spp)
+# only NA! good
+
+# add in site and treatment cols
+codom_spp_lost_siteplot_count_allT <- left_join(codom_spp_lost_siteplot_count_allT, codom_trt_lookup)
+codom_lost_spp_siteplot_names_df <- left_join(codom_lost_spp_siteplot_names_df, codom_trt_lookup) %>%
+  left_join(codom_spp_lookup, by = c("lost_spp" = "spp"))
+
+# plot
+codom_loss_fig <- ggplot(subset(codom_spp_lost_siteplot_count_allT, !grepl("B",plot)), aes(year, lost_spp)) +
+  geom_jitter(alpha = 0.6, width = 0.25, height = 0) +
+  geom_smooth(se = F) + #fill = "deepskyblue1"
+  labs(title = "NWT LTER: number of species permanently lost in codom experiment, by plot, with each sampling event",
+       subtitle = "Points within event number jittered slightly; blue = plot-level trend, purple = meta community species loss and trend",
+       y = "Number of species lost",
+       x = "Monitoring year") +
+  scale_x_continuous(breaks = seq(2002, 2018, by = 2)) +
+  #scale_y_continuous(breaks = seq(0,36,4)) + #expand = c(0.02,0.01), limits = c(0,NA), 
+  theme_light() +
+  facet_grid(plot~.)
+
+
+# -- SPP LOST FROM ENTIRE SYSTEM WITH TIME -----
+# initiate empty master data frame
+codom_spp_lost_meta_counts_allT <- data.frame()
+codom_lost_spp_meta_names_df <- data.frame()
+for(p in unique(codom_veg$plot)){
+  print(paste("looking at", p, "species lost from meta community..."))
+  # create data frame for meta community
+  meta_df <- codom_veg[codom_veg$plot == p,] %>%
+    mutate(event = as.numeric(as.factor(year))) %>%
+    filter(!(rem == "D" & spp == "DESCAE")) %>% # don't track descae in d removal plots
+    filter(!(rem == "A" & spp == "GEUROS")) # don't track geuros in a removal plots
+  
+  
+  # create temporary df for storing new spp counts
+  temp_df <- data.frame(event = unique(meta_df$event), year = unique(meta_df$year))
+  temp_df$plot <- p
+  temp_df$lost_spp <- NA
+  spplist_lag1 <- unique(meta_df$spp[meta_df$event ==1 & meta_df$plot == p])
+  
+  i <- 2
+  while(i %in% 2:max(meta_df$event)){
+    # grab full spp list for next year to end of sampling record
+    fwd_spplist <- unique(meta_df$spp[meta_df$event %in% (i+1):max(meta_df$event)])
+    
+    # grab native spp list for current sampling event
+    event_spp <- unique(meta_df$spp[meta_df$event == i & meta_df$plot == p])
+    # compare spp that disappeared from year prior
+    event_spp_loss <- spplist_lag1[!spplist_lag1 %in% event_spp]
+    # compare spp lost from year prior to future species list
+    event_spp_loss <- event_spp_loss[!event_spp_loss %in% fwd_spplist]
+    print(paste("Event", i, p, "spp lost: ", stringr::str_flatten(event_spp_loss, collapse = ", ")))
+    
+    # store native spp lost
+    if(length(event_spp_loss) > 0){
+      temp_spp_loss_df <- data.frame(lost_spp = event_spp_loss)
+    } else{
+      temp_spp_loss_df <- data.frame(lost_spp = NA)
+    }
+    temp_spp_loss_df$plot <- p
+    temp_spp_loss_df$year <- unique(meta_df$year[meta_df$event == i])
+    temp_spp_loss_df$event <- i
+    
+    # update spp list to current year
+    spplist_lag1 <- event_spp
+    
+    # add info to temp_df
+    temp_df$lost_spp[temp_df$event == i] <- length(event_spp_loss)
+    
+    # add info to codom_lost_spp_meta_df
+    codom_lost_spp_meta_names_df <- rbind(codom_lost_spp_meta_names_df, temp_spp_loss_df)
+    i <- i+1
+  }
+  # add meta new spp to master new spp counts data frame
+  codom_spp_lost_meta_counts_allT <- rbind(codom_spp_lost_meta_counts_allT, na.omit(temp_df)) # removes event 1 (no spp lost in first yr of sampling)
+}
+
+# check to be sure no codes duplicated
+with(codom_lost_spp_meta_names_df, lapply(split(lost_spp,plot), function(x) x[duplicated(x)])) #only NAs duplicated, which is fine (some yrs didn't lose any spp)
+
+# add metacom trend to nativity loss fig
+codom_loss_fig +
+  geom_point(data = subset(codom_spp_lost_meta_counts_allT, !grepl("B",plot)), aes(year, lost_spp), col = "purple") +
+  geom_smooth(data = subset(codom_spp_lost_meta_counts_allT, !grepl("B",plot)), aes(year, lost_spp), col = "purple", se = F) # fill = "orchid"
+
+
+# write out tables for jane
+write.csv(codom_lost_spp_meta_names_df, "codom_meta_lost_spp.csv", row.names = F)
+write.csv(codom_lost_spp_siteplot_names_df, "codom_plot_lost_spp.csv", row.names = F)
+write.csv(codom_spp_gains_meta_names_df, "codom_meta_spp_gains.csv", row.names = F)
+write.csv(codom_spp_gains_siteplot_names_df, "codom_plot_spp_gains.csv", row.names = F)
+
