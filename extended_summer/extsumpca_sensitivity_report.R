4b10c906cb9fd5c8f14d9140675cca03cf770cf9
caitlintwhite <caitlin.t.white@colorado.edu>
2019-04-10 22:12:00 -0700

    add md with figures

---
+++ extended_summer/extsumpca_sensitivity_report.R
@@ -0,0 +1,21 @@
+#' ---
+#' title: Extended summer PCA sensitivity analysis (preliminary)
+#' author: CTW
+#' date: "`r format(Sys.Date())`"
+#' output: github_document
+#' ---
+#'
+#'
+#' ![Extended Summer PC score over time, by data source](figs/PCAsensitivity_PC1overtime.png)
+#' ![Difference between NWT-derived extended summer score and Jennings-derived extended summer score](figs/PCAsensitivity_deltaPC1_overtime.png)
+#' 
+#' 
+#' ![Extended summer year ordination](figs/PCAsensitivity_yrloadings.png)
+#' ![Extended summer variable loadings](figs/PCAsensitivity_sploadings.png)
+#' 
+#' 
+#' ![Input summer metrics, by data source](figs/PCAsensitivity_summermetrics.png)
+#' ![Difference in summer metric derived from NWT NSF data and Jennings et al. infilled data](figs/PCAsensitivity_metricdelta.png)
+#' 
+#' 
+
---
+++ extended_summer/extsumpca_sensitivity_report.md
@@ -0,0 +1,10 @@
+Extended summer PCA sensitivity analysis (preliminary)
+================
+CTW
+2019-04-10
+
+![Extended Summer PC score over time, by data source](figs/PCAsensitivity_PC1overtime.png) ![Difference between NWT-derived extended summer score and Jennings-derived extended summer score](figs/PCAsensitivity_deltaPC1_overtime.png)
+
+![Extended summer year ordination](figs/PCAsensitivity_yrloadings.png) ![Extended summer variable loadings](figs/PCAsensitivity_sploadings.png)
+
+![Input summer metrics, by data source](figs/PCAsensitivity_summermetrics.png) ![Difference in summer metric derived from NWT NSF data and Jennings et al. infilled data](figs/PCAsensitivity_metricdelta.png)
