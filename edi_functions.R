a717040c68aaee378175acc7b1ab96fd78189aff
caitlintwhite <caitlin.t.white@colorado.edu>
2019-06-04 11:12:04 -0600

    modify so same as functions on long-term-trends trends repo

--- edi_functions.R
+++ edi_functions.R
@@ -1,25 +1,26 @@
 # commonly used functions for reading in edi data portal data
 
-# SCE + CTW code to determine most recent version of package ID and read in current dataset on EDI
-#function to determine current version of data package on EDI
-getCurrentVersion<-function(edi_id){
-  versions=readLines(paste0('https://pasta.lternet.edu/package/eml/knb-lter-nwt/', edi_id), warn=FALSE)
+# function to determine data package version number only (not return full data package ID)
+getPackageVersion<-function(edi_id, site = "nwt"){
+  versions=readLines(paste0('https://pasta.lternet.edu/package/eml/knb-lter-', site, '/', edi_id), warn=FALSE)
   currentV <- max(as.numeric(versions))
   return(currentV)
 }
-# function to get entity ID for current version
-getEntityId <- function(edi_id, version){
-  entID <- readLines(paste0('https://pasta.lternet.edu/package/eml/knb-lter-nwt/', edi_id, "/", version, "/"), warn=FALSE)[1]
+
+# function to get entity ID for current data package version
+getEntityId <- function(edi_id, version, site = "nwt", datanum = 1){
+  entID <- readLines(paste0('https://pasta.lternet.edu/package/eml/knb-lter-', site, '/', edi_id, "/", version, "/"), warn=FALSE)[datanum]
   entID <- gsub(paste0("http.*/",edi_id,"/",version,"/"), "", entID) # remove all chars except what comes after last /
   return(entID)
 }
-# reads in tabular dataset for data package that has only one csv data file (could make more generic with read table, but should know what you're reading in to use)
-getTabular <- function(edi_id, na_vals = c("", "NA", NA, NaN, ".", "NaN", " ")){
-  v <- getCurrentVersion(edi_id)
-  id <- getEntityId(edi_id, v)
-  dat <- read.csv(paste0("https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-nwt.", edi_id, ".", v, 
+
+# function to read in tabular csv dataset for data package (could make more generic with read table, but should know what you're reading in to use)
+getTabular <- function(edi_id, na_vals = c("", "NA", NA, NaN, ".", "NaN", " "), site = "nwt", datanum = 1){
+  v <- getPackageVersion(edi_id, site = site)
+  id <- getEntityId(edi_id, v, site = site, datanum = datanum)
+  dat <- read.csv(paste0("https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-", site, ".", edi_id, ".", v, 
                          "&entityid=", id),
                   strip.white =TRUE, na.strings = na_vals)
-  print(paste0("Reading in knb-lter-nwt.", edi_id, ".", v))
+  print(paste0("Reading in knb-lter-", site, ".", edi_id, ".", v))
   return(dat)
 }
