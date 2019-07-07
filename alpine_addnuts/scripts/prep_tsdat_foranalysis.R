cbb4d8a9c418f9662db8a8c431d0a125c05d5156
caitlintwhite <caitlin.t.white@colorado.edu>
2019-07-06 17:18:22 -0700

    add in most recent 2016 codes from ts

--- alpine_addnuts/scripts/prep_tsdat_foranalysis.R
+++ alpine_addnuts/scripts/prep_tsdat_foranalysis.R
@@ -4,6 +4,7 @@
 # read in all ts alpine plant comm dats, spp lookup table, perhaps marko trait dataset
 # standardize spp names across all datasets
 # standardize site descriptive info for plots across years (e.g. all plot 1 at saddle should have same meadow, snow and N/P nut addition info)
+# > note: add in 2016 sdl descrip as alternative (ts sent to ctw on 7/5/19)
 # create spp matrices for NMDS:
 # 1) top hit only -- update: can't do this bc some raw datasets have vert height summed already
 # 2) all hits
@@ -34,10 +35,12 @@ nutnet17 <- read.csv(datfiles[grep("net17[.]", datfiles)], strip.white = T, na.s
 nutnet17raw <- read.csv(datfiles[grep("net17r", datfiles)], strip.white = T, na.strings = na_vals)
 
 ## Sdl 
-plot_codes <- read.csv(datfiles[grep("codes", datfiles)], strip.white = T, na.strings = na_vals)
+plot_codes <- read.csv(datfiles[grep("codes_99", datfiles)], strip.white = T, na.strings = na_vals)
 sdl1997 <- read.csv(datfiles[grep("1997", datfiles)], strip.white = T, na.string = na_vals)
 sdl2012 <- read.csv(datfiles[grep("2012", datfiles)], strip.white = T, na.strings = na_vals)
 sdl2016 <- read.csv(datfiles[grep("2016", datfiles)], strip.white = T, na.strings = na_vals)
+# ts 2016 codes sent 7/5
+plots2016 <- read.csv(datfiles[grep("16_codes", datfiles)], strip.white = T, na.string = na_vals)
 
 ## spp lookup table
 spplt <- read.csv("alpine_addnuts/output_data/sdl_nutnet_spplookup.csv", na.strings = na_vals, strip.white = T)
@@ -178,29 +181,46 @@ sdl_plots <- dplyr::select(sdl2016, -c(point, species)) %>%
   dplyr::select(plot, old_plot, meadow, sfcode, trt)
 # append 16 to colnames after plot to keep track of origin
 colnames(sdl_plots)[2:ncol(sdl_plots)] <- paste0(colnames(sdl_plots)[2:ncol(sdl_plots)], 16)
+# join july 2019 2016 codes TS sent to CTW
+sdl_plots <- left_join(sdl_plots, plots2016) %>%
+  #drop empty col in 2016v2 
+  dplyr::select(-X) %>%
+  rename(notes = X.1)
+#id pos that ends with last concatenation
+pos <- max(grep("16", colnames(sdl_plots)))
+# append "16.2" to cols joined and lowercase colnames
+colnames(sdl_plots)[(pos+1):ncol(sdl_plots)] <- paste0(casefold(colnames(sdl_plots[(pos+1):ncol(sdl_plots)])), "16.2")
+
 # join plot_codes lookup data
 sdl_plots <- full_join(sdl_plots, plot_codes, by = "plot") 
+#id pos that ends with last concatenation
+pos <- max(grep("16.2", colnames(sdl_plots)))
 # append "LT" to colnames added to keep track of origin
-colnames(sdl_plots)[6:ncol(sdl_plots)] <- paste0(colnames(sdl_plots)[6:ncol(sdl_plots)], "LT")
+colnames(sdl_plots)[(pos+1):ncol(sdl_plots)] <- paste0(colnames(sdl_plots)[(pos+1):ncol(sdl_plots)], "LT")
 # > none of the old_plot code in the plot_code table match up with old_plot in the sdl16 dataset!!
 # > BUT! trtment and snow cols match up so that's good. be sure to ignore old. in 2016 dataset when tidying it
 
 # join 2012 plot data
 sdl_plots <- full_join(sdl_plots, distinct(dplyr::select(sdl2012, meadow:plot)), by = "plot")
+#id pos that ends with last concatenation
+pos <- max(grep("LT", colnames(sdl_plots)))
 # append 12 to colnames added to keep track of origin
-colnames(sdl_plots)[10:ncol(sdl_plots)] <- paste0(colnames(sdl_plots)[10:ncol(sdl_plots)], 12)
+colnames(sdl_plots)[(pos+1):ncol(sdl_plots)] <- paste0(colnames(sdl_plots)[(pos+1):ncol(sdl_plots)], 12)
 View(sdl_plots) # there are some plots in the 2012 dataset that aren't in the 2016 dataset..
 
 # join sdl 1997
 sdl_plots <- full_join(sdl_plots, dplyr::select(sdl1997, old_plot, site, trt), by = c("old_plotLT" = "old_plot"))
+#id pos that ends with last concatenation
+pos <- max(grep("12", colnames(sdl_plots)))
 # append 97 to colnames added to keep track of origin
-colnames(sdl_plots)[13:14] <- paste0(colnames(sdl_plots)[13:14], 97)
+colnames(sdl_plots)[(pos+1):ncol(sdl_plots)] <- paste0(colnames(sdl_plots)[(pos+1):ncol(sdl_plots)], 97)
 
 # reorg colnames so cols that should match up are adjacent
 sdl_plots <- dplyr::select(sdl_plots,
-                           plot, old_plotLT, old_plot16, meadowLT, meadow12, meadow16,
-                           snowLT, site97, snow12, sfcode16,
-                           trtLT, trt97, fert12, trt16) %>%
+                           plot, old_plotLT, old_plot16, 
+                           meadowLT, meadow12, meadow16, meadow16.2,
+                           snowLT, site97, snow12, snow16.2, sfcode16, recovery16.2, notes16.2,
+                           trtLT, trt97, fert12, trt16, fert16.2) %>%
   arrange(plot) %>%
   distinct()
   #rm extra old plotnums in the 2016 that pair with plot 14 and aren't 287 since there is a match for that
@@ -295,21 +315,74 @@ checkdups <- master_plant %>%
 summary(checkdups$nobs) # all 1s -- each spp only has one entry per plot. good! ready to write out
 
 
-# clean up site lookup table
+
+# --- CLEAN UP SITE LOOKUP TABLES -----
 # add site to nutnet lookup
 nutnet_plots <- mutate(nutnet_plots, site = "nutnet") %>%
   dplyr::select(site, plotid:ncol(.))
 
-# select plot and treatment from sdl (choose sdl 16 bc that has the most)
+
+# specify dry meadow codes used across sdl datasets
+drycodes <- c("d", "Dry", "dry")
+wetcodes <- c("w")
+mescodes <- c("m", "mesic")
+snowcodes <- c("snow", "s", "SD")
+nosnow <- c("n", "none", "recovery", "NONE", "ND")
+
+
+# select plot and treatment from sdl (choose sdl 16.2 bc that is most recent info from TS)
 sdl_plots2 <- sdl_plots %>%
-  mutate(trt = ifelse(is.na(trt16), trt97, trt16),
+  mutate(trt = ifelse(is.na(fert16.2), trt97, fert16.2),
          trt = ifelse(is.na(trt), fert12, trt),
          trt = casefold(trt, upper = T),
          trt = recode(trt, "NN" = "N", "CC" = "C", "CONTROL" = "C",
-                      "PP" = "P", "NP" = "N+P")) %>%
+                      "PP" = "P", "NP" = "N+P"),
+         sfcode2016 = ifelse(!is.na(recovery16.2), recovery16.2,
+                         ifelse(snow12 == "n", 1, NA)),
+         sfcode2016 = ifelse(is.na(sfcode2016), "Unknown", sfcode2016),
+         notes2016 = notes16.2,
+         notes2016 = ifelse(is.na(notes2016) & sfcode2016 == 1, "never affected", notes2016),
+         notes2016 = ifelse(is.na(notes2016), "Unknown", notes2016)) %>%
   rename(old_plot = old_plot97) %>%
-  dplyr::select(plot, old_plot, trt) %>%
-  ungroup() %>% distinct()
+  #temp create rowid for grouping by row
+  mutate(rowid = row.names(.)) %>%
+  group_by(rowid) %>%
+  mutate(meadow = ifelse(meadowLT %in% c(drycodes, NA) &
+                           meadow12 %in% c(drycodes, NA) &
+                           meadow16 %in% c(drycodes, NA), "Dry", "Unknown"),
+         meadow = ifelse(meadowLT %in% c(wetcodes, NA) &
+                           meadow12 %in% c(wetcodes, NA) &
+                           meadow16 %in% c(wetcodes, NA), "Wet", meadow),
+         meadow = ifelse(meadowLT %in% c(mescodes, NA) &
+                           meadow12 %in% c(mescodes, NA) &
+                           meadow16 %in% c(mescodes, NA), "Mesic", meadow),
+         #correct for NAs in all
+         meadow = ifelse(is.na(meadowLT) & is.na(meadow12) & is.na(meadow16), "Unknown", meadow),
+         # specify snow col
+         snow = ifelse(snowLT %in% c(snowcodes, NA) &
+                         site97 %in% c(snowcodes, NA) &
+                         snow12 %in% c(snowcodes, NA) &
+                         sfcode16 %in% c(snowcodes, NA), "Snow", "Unknown"),
+         snow = ifelse(snowLT %in% c(nosnow, NA) &
+                         site97 %in% c(nosnow, NA) &
+                         snow12 %in% c(nosnow, NA) &
+                         sfcode16 %in% c(nosnow, NA), "No snow", snow),
+         #correct for NAs in all
+         snow = ifelse(is.na(snowLT) & is.na(site97) & is.na(snow12) & is.na(sfcode16), "Unknown", snow)) %>%
+  ungroup() %>%
+  # add in alternate meadow and snow cols based on most recent ts 2016 info
+  mutate(meadow_alt = ifelse(!is.na(meadow16.2), meadow16.2, meadow),
+         snow_alt = ifelse(!is.na(snow16.2), snow16.2, snow),
+         # correct code spellings for snow_alt
+         snow_alt = gsub("SNOW", "Snow", snow_alt),
+         snow_alt = gsub("NONE", "No snow", snow_alt)) %>%
+  #retain cols just used for analysis
+  #retain reference cols for analysis only
+  dplyr::select(plot, old_plot, trt, meadow, meadow_alt, snow, snow_alt, sfcode2016, notes2016) %>%
+  data.frame()
+
+# after review, old_plot 96 is probably old_plot 296, so change descriptive info to match that
+sdl_plots2[sdl_plots2$old_plot == 96 & !is.na(sdl_plots2$old_plot),c(1,3:ncol(sdl_plots2))] <- sdl_plots2[sdl_plots2$old_plot == 296 & !is.na(sdl_plots2$old_plot),c(1,3:ncol(sdl_plots2))]
 
 
 
