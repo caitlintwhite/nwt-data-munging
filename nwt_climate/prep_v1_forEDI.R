b7cf8e414b56341d8e3c3e2da17236b2e8f3e858
Caitlin White <caitlin.t.white@colorado.edu>
2023-05-20 20:15:28 -0600

    finish prep temp v1 for EDI

--- nwt_climate/prep_v1_forEDI.R
+++ nwt_climate/prep_v1_forEDI.R
@@ -28,9 +28,13 @@ snotelppt <- readRDS(qcdats[grepl("telP", qcdats)])
 nwtlogtemp <- readRDS(qcdats[grepl("loggerT", qcdats)])
 allppt <- readRDS(paste0(datpath, "qc/siteinfoPPT_qc.rds"))
 
-# read in kittel datasets to check colnames
+# read in kittel datasets to check colnames and station names
 tkd1_temp <- getTabular(185, datanum = 1)
 tkd1_ppt <- getTabular(186)
+
+# read in keith's dataset to see colname convention and infill station name convention
+jennings <- getTabular(168)
+# > also notice in metadata for that dataset they only used 1952-2010 infilled c1 and d1 precip.. revisit how summed hourly values compare to my infilled dailies
 
 # what sites were temporally available to use for sdl temp?
 # 1986-2017 = loggers
@@ -127,6 +131,36 @@ subset(snoteltemp, yr > 1986, select = c(station_id, station_name)) %>%
 # > going to leave airtemp_max/min/avg as is and dtr lowcase (not following C1 and D1 colnames, will suggest to sarah we adjust those)
 # > not going to put "num_obs...regression_equation" because will be too long for hmps (end it at "..regression")
 
+names(tkd1_temp)
+sapply(tkd1_temp[grepl("LTER|local|flag|source", names(tkd1_temp))], function(x) sort(unique(x)))
+names(jennings)
+sapply(jennings[grepl("LTER|local|flag", names(jennings))], function(x) sort(unique(x)))
+
+# keith's flag explanations:
+# The flagging structure has the format qcXinY, 
+# where X indicates the quality control protocol applied, if any, 
+# and Y indicates the infilling protocol applied, if any
+
+# X can have the following values: 
+## 0 (observation passed all quality control checks); 
+## 1 (observation removed because it falls outside of the maximum/minimum threshold for that variable); 
+## 2 (observation removed because it exceeded the rate of change threshold); 
+## 3 (observation removed because the sensor was stuck); 
+## 4 (variable missing from raw data due to power outage, missing files, datalogger issue, etc.).
+
+# Y can have the following values: 
+## 0 (no infilling applied); 
+## 1 (gap = 1 h, infilled by using linear interpolation between previous and following observations);
+## 2 (gap > 1 h and <= 24, infilled by taking average value of observation 24 hours before and after missing value); 
+## 3 (gap > 24 and <= 72 h, infilled with forecasted and backcasted ARIMA model); 
+## 4 (infilled using multiple linear regression of observations from both other met stations); 
+## 5 (infilled using linear regression from closest met station); 
+## 6 (infilled using linear regression from farthest met station); 
+## 7 (infilled using climatic mean for that month and 3 h time block); 
+## ns (quality flags not specified) [ctw note: for 2014 onwards when kehoe infilled the data]
+
+
+# -- 1. PRETTY SDL TEMP -----
 sdl_temp_pretty <- sdl_temp
 names(sdl_temp_pretty) <- gsub("pval$|pval(?=_)" , "pvalue", names(sdl_temp), perl = T)
 names(sdl_temp_pretty) <- gsub("r2" , "rsquared", names(sdl_temp_pretty))
@@ -141,14 +175,197 @@ sdl_temp_pretty <- subset(sdl_temp_pretty, select = c(LTER_site:logger, year, da
                                      flag_1:ncol(sdl_temp_pretty)))
                           
                                      
-# make sure flags are correct
+# -- clean up regression flags ----
+# revisit sdl flags again..
+sapply(sdl_temp[grepl("flag|note", names(sdl_temp))], function(x) sort(unique(x)))
+# > change B* in flag_2 for temp to B. Doesn't matter to user than the raw adjustment failed. Either way used predicted DTR and predicted mean to derive tmax and tmin.
+# maybe note in qc_flag that prediction chosen over observed-adjusted
+# which rows have B*
+View(subset(sdl_temp_pretty, flag_2 == "B*"))
+# these both happened in observed tmax cases. for one, tmin pulled bc outside sensor range, the other date's tmin was absent
+sdl_temp_pretty$airtemp_max_qcflag[grepl("B[*]", sdl_temp_pretty$flag_2)] <- "Predicted daily temperatures chosen over values derived from observed Tmax"
+# change asterisk to B
+sdl_temp_pretty$flag_2[grepl("B[*]", sdl_temp_pretty$flag_2)] <- "B"
+sapply(sdl_temp_pretty[grepl("flag_[0-9]", names(sdl_temp_pretty))], function(x) sort(unique(x))) # all regression flags good now
 
 
-# QAQC notes should be intelligible
+# -- clean up QC notes -----
+sapply(sdl_temp_pretty[grepl("qcflag|note", names(sdl_temp_pretty))], function(x) sort(unique(x)))
+# look at observations where tmean removed due to max or min not recorded
+View(subset(sdl_temp_pretty, grepl("not recorded", airtemp_avg_qcflag)))
+# > funny: the predicted means are close in val to observed tmeans, esp for 1994-04-08 (both 11C), even tho tmax and tmin not recorded those days
+
+# QAQC notes should be intelligible and standardized
+sdl_temp_flagging <- subset(sdl_temp) %>%
+  subset(select = grepl("date|yr|mon|logger|flag", names(.)))
+# separate qc flagging
+sdl_temp_qcflagging <- sdl_temp_flagging %>%
+  subset(select = grepl("date|yr|mon|logger|qc", names(.))) %>%
+  gather(met, val, airtemp_max_qcflag:ncol(.)) %>%
+  mutate(airtemp = str_extract(met, "airtemp_min|airtemp_max|airtemp_avg"),
+         met = gsub("^a.*_max_|^a.*_avg_|^a.*_min_", "", met)) %>%
+  distinct() %>%
+  # keep only records with flags (value NA'd)
+  subset(!is.na(val))
+# review:
+with(sdl_temp_qcflagging, sapply(split(val, paste(logger, met)), function(x) summary(factor(x))))
+
+# > at this time, can't differentiate within-site from cross-site sd violation. within site compared triplicate sensors at some station, cross site compared local_sites (sdl v d1 v c1) 
+# or because there are so few hmp flags, maybe just see what they all are
+
+subset(sdl_temp_qcflagging, grepl("or spatial coh", val)) # these should be clarified, pre-hmp will probably be spatial coherency; hmp could be either
+# for time's sake, run gapfill_sdlts.R to find intermediate data frame that shows exact issue
+# > create hmp_dailysds to execute following code:
+
+View(subset(hmp_dailysds, station_id == "sdl" & date %in% c(sdl_temp_qcflagging$date[grepl("hmp1", sdl_temp_qcflagging$met)])))
+# hmp1 flagged for xs violation
+# 2021-02-09: flagged for top 15 xs rate change within tmax across all sites (including its own hmps)
+# 2021-08-26: flagged for xs temp and xs rate change in tmin (every other data point 1.6-3.4 and hmp1 was -3.6)
+
+View(subset(hmp_dailysds, station_id == "sdl" & date %in% c(sdl_temp_qcflagging$date[grepl("hmp3", sdl_temp_qcflagging$met)])))
+# hmp 3 also flagged for xs:
+# 2012-12-23: top 15 rate change on tmin
+# 2021-05-21: top 15 rate change on tmax (except predicted value made rate change worse -- need to write checks for this in the future. amount off compared to others is ~0.5 so whatever. predicion made delta +1.34 instead of original +1.39 [others were +1.8-1.9])
+# > maybe if value is within 0.5 [or 1?] of original, keep original
+# 2021-06-06: top 15 xs daily temp violation on tmin (legit): everything else c1 to d1 and other sdl hmps was around 3-3.5, hmp3 was +6
+# > but prediction is a bit low of others (1.6 from source sdl hmp2.. averaged tmin for the day ends up being 2.5.. which, other sensors were around 3)
+
+# conversions:
+# comp deviance & spatial coherence dev = "Value removed for spatial coherence deviation in rate change and/or daily value" [I actually don't know if 'comp deviance' is for daily value or rate change]
+# sensor fail = "Value removed, sensor failure"
+# high value = "High value, value removed for spatial coherence deviation"
+# within-site = "Value removed for same-site triplicate sensor deviation in rate change and/or daily value"
+
+# need to do manual corrections since flags are in wide-col format
+# make a data frame with each unique flag, and it's cleaned up name
+cr_qcflags <- sapply(sdl_temp_pretty[grepl("qcflag$", names(sdl_temp_pretty))], function(x) sort(unique(x)))
+cr_qcflags <- unlist(cr_qcflags)
+cr_qcflags <- unname(cr_qcflags)
+# remove Predicted value selected bc I just added that (it's fine)
+cr_qcflags <- unique(cr_qcflags[!grepl("Predicted daily temp", cr_qcflags)])
+cr_qcflags <- data.frame(original = cr_qcflags)
+
+# make clean flag LUT
+## flag for comp deviance & any cr logger that has within-site or spatial coherence
+cr_qcflags$replacement <- "Value removed for spatial coherence deviation in rate change and/or daily value"
+# high value
+cr_qcflags$replacement[grepl("^high", cr_qcflags$original)] <- "High value, removed for spatial coherence deviation"
+# shifting temp by x days
+cr_qcflags$replacement[grepl("shifted", cr_qcflags$original)] <- gsub("shifted temp", "Temperature value shifted", cr_qcflags$original[grepl("shifted", cr_qcflags$original)])
+# sensor fail
+cr_qcflags$replacement[grepl("^sensor", cr_qcflags$original)] <- "Value removed, sensor failure"
+# mean temp removed bc daily extreme failed, not recorded (keep as is just capitalize first letter in note)
+cr_qcflags$replacement[grepl("daily average temp ", cr_qcflags$original)] <-  gsub("daily average temp ", "Average temperature value ", cr_qcflags$original[grepl("daily average temp", cr_qcflags$original)])
+cr_qcflags$replacement[grepl("not recorded", cr_qcflags$original)] <-  "Average temperature value removed: Tmax and Tmin not recorded"
+# capitalize 't' in Tmin, Tmax 
+cr_qcflags$replacement <- gsub(" tm", " Tm", cr_qcflags$replacement)
+# see how it looks
+cr_qcflags # okay
+# can clean up hmp1 and 3 manually since only 5 cases
+
+# iterate through cr qc_flag notes and replace
+for(t in c("max", "min", "avg")){
+  tempcol <- paste0("airtemp_", t, "_qcflag")
+  for(i in 1:nrow(cr_qcflags)){
+    sdl_temp_pretty[[tempcol]][grepl(cr_qcflags$original[i], sdl_temp_pretty[[tempcol]])] <- cr_qcflags$replacement[i]
+  }
+}
+
+# check flags
+sapply(sdl_temp_pretty[grepl("qcflag|note", names(sdl_temp_pretty))], function(x) sort(unique(x))) # seems like it's okay
+
+# manually correct hmps
+## hmp1
+sapply(sdl_temp_pretty[grepl("qcflag_hmp1", names(sdl_temp_pretty))], function(x) sort(unique(x)))
+# hmp1 flagged for xs violation:
+# > 2021-02-09: flagged for top 15 xs rate change within tmax across all sites (including its own hmps)
+sdl_temp_pretty$airtemp_max_qcflag_hmp1[grepl("within", sdl_temp_pretty$airtemp_max_qcflag_hmp1)] <- "Value removed for spatial coherence deviation in rate change"
+
+# > 2021-08-26: flagged for xs temp and xs rate change in tmin (every other data point 1.6-3.4 and hmp1 was -3.6)
+sdl_temp_pretty$airtemp_min_qcflag_hmp1[grepl("within", sdl_temp_pretty$airtemp_min_qcflag_hmp1)] <- "Value removed for spatial coherence deviation in rate change and daily value"
+
+## hmp3
+# hmp 3 flagged for xs:
+# > 2020-12-23: top 15 rate change on tmin
+sdl_temp_pretty$airtemp_min_qcflag_hmp3[grepl("within", sdl_temp_pretty$airtemp_min_qcflag_hmp3) & sdl_temp_pretty$date == as.Date("2020-12-23")] <- "Value removed for spatial coherence deviation in rate change"
+
+# > 2021-02-05: top 15 rate change on tmax
+sdl_temp_pretty$airtemp_max_qcflag_hmp3[grepl("within", sdl_temp_pretty$airtemp_max_qcflag_hmp3) & sdl_temp_pretty$date == as.Date("2021-02-05")] <- "Value removed for spatial coherence deviation in rate change"
+
+# > 2021-06-06: top 15 xs daily temp violation on tmin
+sdl_temp_pretty$airtemp_min_qcflag_hmp3[grepl("within", sdl_temp_pretty$airtemp_min_qcflag_hmp3) & sdl_temp_pretty$date == as.Date("2021-06-06")] <- "Value removed for spatial coherence deviation in daily value"
+
+# check flags again
+sapply(sdl_temp_pretty[grepl("flag|note", names(sdl_temp_pretty))], function(x) sort(unique(x))) # looks okay
 
 
+
+# -- clean up station names ----
 # source station names should be clear
+sapply(sdl_temp_pretty[grepl("source", names(sdl_temp_pretty))], function(x) sort(unique(x))) 
+
+# create station LUT, iterate through each source col and replace as did for qc notes
+# > naming convention on EDI for loggers is SITE data logger CR[x]
+# > D1 and C1 gap-filled have lowcase loggers, but will follow how it's shown on EDI
+# Snotel has: Name (number)
+# GHCDN is last 5 digits of station ID
+unique(sdl_temp_pretty$source_station)
+
+temp_station_LUT <- distinct(sitestemp[c("station_id", "station_name", "local_site", "source")]) %>%
+  mutate(pretty_name = ifelse(source == "nwt lter", casefold(gsub("_", " ", station_id), upper = T),
+                              ifelse(source == "snotel", paste0(station_name, " (", station_id, ")" ),
+                                     ## gsub("US.00", "", station_id))),
+         # I don't think I'm gonna sub out the first part of ghcnd stations like TK did because otherwise it will convert them to numeric
+         station_id)),
+         # little adjustments for nwt
+         pretty_name = gsub("CHART", "chart", pretty_name),
+         pretty_name = gsub(" CR", " logger CR", pretty_name),
+         pretty_name = gsub("HMP ", "HMP-", pretty_name))
 
 
-# remove mon, doy, pay attention to letter casing
+# iterate through cr qc_flag notes and replace
+for(n in names(sdl_temp_pretty)[grepl("source", names(sdl_temp_pretty))]){
+  for(i in 1:nrow(temp_station_LUT)){
+    sdl_temp_pretty[[n]][(sdl_temp_pretty[[n]] == temp_station_LUT$local_site[i]) & !is.na(sdl_temp_pretty[[n]])] <- temp_station_LUT$pretty_name[i]
+  }
+}
 
+# check names
+sapply(sdl_temp_pretty[grepl("source", names(sdl_temp_pretty))], unique) # looks good
+
+
+
+# -- finalize dataset -----
+# check cols and arrangement: remove mon, doy, pay attention to letter casing
+str(sdl_temp_pretty) # colnames look okay
+
+# final check of ranges and char values
+summary(sdl_temp_pretty)
+summary(is.na(sdl_temp_pretty))
+View(subset(sdl_temp_pretty, !is.na(raw_airtemp_avg_flag_hmp3))) # I forgot hmps have raw flags of "n" when no flagging assigned (why more non-NA flags than there are non-NA data values)
+sapply(sdl_temp_pretty[grepl("LTER|local|logg|flag|source",names(sdl_temp_pretty))], function(x) sort(unique(x)))
+# still need to capitalize local_site and logger
+
+sdl_temp_pretty$local_site <- casefold(sdl_temp_pretty$local_site, upper = T)
+sdl_temp_pretty$logger <- casefold(sdl_temp_pretty$logger, upper = T)
+sdl_temp_pretty$logger <- gsub("_HMP", " HMP", sdl_temp_pretty$logger)
+
+# be sure no duplicate dates and all dates accounted for
+summary(seq.Date(min(sdl_temp_pretty$date), max(sdl_temp_pretty$date), 1) %in% sdl_temp_pretty$date)
+summary(duplicated(sdl_temp_pretty$date)) # good
+
+#check decimal places on saddle temp pretty.. (noticing weird running decimal places on homogenized DTR)
+sapply(sdl_temp_pretty[grepl("_homogenized$", names(sdl_temp_pretty))], function(x) head(x, n = 20))
+sapply(sdl_temp_pretty[grepl("_gapfilled$", names(sdl_temp_pretty))], function(x) head(x, n = 20))
+
+# I think temp ready to go
+sapply(sdl_temp_pretty, class) # col classes seem right
+summary(is.na(sdl_temp_pretty)) # some flag columns only have a few flag values. read_csv reads in these as logical classes and it throws errors, so just let sarah know
+# i don't want to quote NA cells bc then numerics will read in as character and that's annoying. just tell data users to pay attn to col types in metadata
+sapply(sdl_temp_pretty[grepl("source", names(sdl_temp_pretty))], unique)
+View(subset(sdl_temp_pretty, source_station_hmp2 == "University Camp (838)")) # just to see why hmp1 and 3 were't used (answer: they were also out)
+# looks fine overall
+
+
+# -- write out pretty temp -----
+write.csv(sdl_temp_pretty, paste0(datpath, "publish/sdl_daily_airtemp_gapfilled_ongoing.csv"), row.names = F)
